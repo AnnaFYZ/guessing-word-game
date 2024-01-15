cfd73db4b0d70305e00f8cd3ba96cee79832040e
Olha Danylevska <odanilevskaya2@gmail.com>
2024-01-15 22:20:22 +0000

    node modules

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Timer from './timer';
+import Timer from './components/timer';
 import Input from './components/input-component';
 
 function App() {
   return (
     <div className="App">
       <Input />
+      <Timer />
     </div>
   );
 }
