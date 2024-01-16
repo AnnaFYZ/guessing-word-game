1ab7cfffb03b1dcafa1dd2f159fcc6359746a87f
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2024-01-16 22:18:58 +0000

    get random word function created

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Timer from './components/timer';
+import Input from './components/input-component';
+import Card from './components/card';
+import { Box } from '@mui/system';
+import { useState } from "react";
+
+
+function App() {
+  //the states for letterState (pending, guessed, wrong)
+  const [letterState, setLetterState] = useState("wrong");
+
+  return (
+    <div className="App">
+      <Box
+        sx={{
+          width: "100%",
+          height: "100vh",
+          backgroundImage: "linear-gradient(to bottom right, #720E7A, #0F1A37)",
+        }}
+      >
+        <Timer />
+        <Card letterState={"guessed"} />
+        <Input />
+        <Card letterState={letterState} />
+      </Box>
+    </div>
+  );
+}
+
+
+export default App;
