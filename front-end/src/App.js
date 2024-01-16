b49f8b19c5e65a7fc3ab9173a1fb1b6b7a4cf25d
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2024-01-16 20:27:38 +0000

    Routing
     - Created Home.jsx, where I added the name of the game, a short description (to be updated), 2 buttons, and 3 icons.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Timer from './timer';
+import Timer from './components/timer';
 import Input from './components/input-component';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Input />
-      <Timer />
+      <Router>
+        <Home />
+        <Input />
+        <Timer />
+      </Router>
     </div>
   );
 }
