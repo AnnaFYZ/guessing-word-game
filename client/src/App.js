a6ab6aa5df1196b381315d5bc8e1c81a0874654e
Olha Danylevska <odanilevskaya2@gmail.com>
2024-01-17 17:31:43 +0000

    fixed router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Timer from './components/timer';
 import Input from './components/input-component';
-import Card from './components/card';
 import { Box } from '@mui/system';
 import { useState } from "react";
 import DummyWordHolder from './components/dummyWordHolder';
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home/Home"
+
 
 
 function App() {
@@ -13,16 +14,19 @@ function App() {
 
   return (
     <div className="App">
-      <Box
-        sx={{
-          width: "100%",
-          height: "100vh",
-          backgroundImage: "linear-gradient(to bottom right, #720E7A, #0F1A37)",
-        }}
-      >
-        <Input />
-        <DummyWordHolder letterState={letterState} setLetterState={setLetterState} />
-      </Box>
+      <Router>
+        <Home />
+        <Box
+          sx={{
+            width: "100%",
+            height: "100vh",
+            backgroundImage: "linear-gradient(to bottom right, #720E7A, #0F1A37)",
+          }}
+        >
+          <Input />
+          <DummyWordHolder letterState={letterState} setLetterState={setLetterState} />
+        </Box>
+      </Router>
     </div>
   );
 }
