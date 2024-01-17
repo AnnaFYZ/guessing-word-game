1a5b197c3442d36643146f4f3f52b453c37c5149
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2024-01-17 03:55:41 +0000

    Updated Home.jsx
    - Added Box and Typography.
    - Added Icons.
    - Items were centered.
    - Added a short description.
    - Moved buttons from App.js to here.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Timer from './components/timer';
 import Input from './components/input-component';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router } from "react-router-dom";
 import Card from './components/card';
 import { Box } from '@mui/system';
 import { useState } from "react";
+import MyButton from './components/button';
 
 
 function App() {
@@ -12,18 +15,23 @@ function App() {
 
   return (
     <div className="App">
-      <Box
-        sx={{
-          width: "100%",
-          height: "100vh",
-          backgroundImage: "linear-gradient(to bottom right, #720E7A, #0F1A37)",
-        }}
-      >
-        <Timer />
-        <Card letterState={"guessed"} />
-        <Input />
-        <Card letterState={letterState} />
-      </Box>
+      <Router>
+        <Home />
+        <Box
+          sx={{
+            width: "100%",
+            height: "100vh",
+            backgroundImage:
+              "linear-gradient(to bottom right, #720E7A, #0F1A37)",
+          }}
+        >
+          <Timer />
+          <Card letterState={"guessed"} />
+          <Input />
+          <MyButton buttonText="Confirm" color="yellow" />
+          <Card letterState={letterState} />
+        </Box>
+      </Router>
     </div>
   );
 }
