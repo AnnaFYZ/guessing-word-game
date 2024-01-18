e9ee35aca6006c8d49027e96e405c4b33940adc9
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2024-01-18 00:46:33 +0000

    Implement Routing for "Start New Game" Feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
-import { Box } from '@mui/system';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/system";
 import { useState } from "react";
-import DummyWordHolder from './components/dummyWordHolder';
-import DummyInputHolder from './components/dummyInputHolder';
-import Home from "./components/Home/Home"
-import HolderForWrongLetters from './components/dummyWrongWords';
-
-
+import DummyWordHolder from "./components/dummyWordHolder";
+import DummyInputHolder from "./components/dummyInputHolder";
+import Home from "./components/Home/Home";
+import HolderForWrongLetters from "./components/dummyWrongWords";
 
 function App() {
   //the states for letterState (pending, guessed, wrong)
@@ -16,24 +14,32 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Home />
-        <Box
-          sx={{
-            width: "100%",
-            height: "100vh",
-            backgroundImage: "linear-gradient(to bottom right, #720E7A, #0F1A37)",
-          }}
-        >
-          <DummyWordHolder letterState={letterState} setLetterState={setLetterState} />
-          <HolderForWrongLetters letterState={letterState} />
-          <DummyInputHolder />
-
-        </Box>
+        <Routes>
+          <Route
+            path="/start-new-game"
+            element={
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "100vh",
+                  backgroundImage:
+                    "linear-gradient(to bottom right, #720E7A, #0F1A37)",
+                }}
+              >
+                <DummyWordHolder
+                  letterState={letterState}
+                  setLetterState={setLetterState}
+                />
+                <HolderForWrongLetters letterState={letterState} />
+                <DummyInputHolder />
+              </Box>
+            }
+          />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </div>
   );
 }
-
-
 
 export default App;
